0d0de62a71a7994172a605024839614ba48d9f21
David Hall <david.hall.physics@gmail.com>
2020-11-20 06:22:28 -0800

    Test sort(), min(), max()

--- tests/testthat/test-vctrs-compare.R
+++ tests/testthat/test-vctrs-compare.R
@@ -44,3 +44,55 @@ test_that("inequality comparisons work", {
     vec_compare(x, NA, na_equal = TRUE)
   )
 })
+
+test_that("sort works", {
+  x <- c(0, -1, 1, NA)
+
+  expect_equal(
+    sort(biginteger(x), decreasing = FALSE, na.last = NA),
+    biginteger(c(-1, 0, 1))
+  )
+  expect_equal(
+    sort(biginteger(x), decreasing = TRUE, na.last = NA),
+    biginteger(c(1, 0, -1))
+  )
+  expect_equal(
+    sort(biginteger(x), decreasing = FALSE, na.last = TRUE),
+    biginteger(c(-1, 0, 1, NA))
+  )
+  expect_equal(
+    sort(biginteger(x), decreasing = FALSE, na.last = FALSE),
+    biginteger(c(NA, -1, 0, 1))
+  )
+
+  expect_equal(
+    sort(bigfloat(x), decreasing = FALSE, na.last = NA),
+    bigfloat(c(-1, 0, 1))
+  )
+  expect_equal(
+    sort(bigfloat(x), decreasing = TRUE, na.last = NA),
+    bigfloat(c(1, 0, -1))
+  )
+  expect_equal(
+    sort(bigfloat(x), decreasing = FALSE, na.last = TRUE),
+    bigfloat(c(-1, 0, 1, NA))
+  )
+  expect_equal(
+    sort(bigfloat(x), decreasing = FALSE, na.last = FALSE),
+    bigfloat(c(NA, -1, 0, 1))
+  )
+})
+
+test_that("min/max works", {
+  x <- c(0, -1, 1, NA)
+
+  expect_equal(min(biginteger(x), na.rm = FALSE), NA_biginteger_)
+  expect_equal(min(biginteger(x), na.rm = TRUE), biginteger(-1))
+  expect_equal(max(biginteger(x), na.rm = FALSE), NA_biginteger_)
+  expect_equal(max(biginteger(x), na.rm = TRUE), biginteger(1))
+
+  expect_equal(min(bigfloat(x), na.rm = FALSE), NA_bigfloat_)
+  expect_equal(min(bigfloat(x), na.rm = TRUE), bigfloat(-1))
+  expect_equal(max(bigfloat(x), na.rm = FALSE), NA_bigfloat_)
+  expect_equal(max(bigfloat(x), na.rm = TRUE), bigfloat(1))
+})
